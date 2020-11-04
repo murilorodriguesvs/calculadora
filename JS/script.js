581c9574968a27ab3ab71d7879145900ead10d65
Murilo Rodrigues <65139977+murilorodriguesvs@users.noreply.github.com>
2020-11-03 21:20:21 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,53 +1,108 @@
-document.addEventListener('DOMContentLoaded', function(){
-    //Display
-    const tela = document.querySelector('#display');
+window.addEventListener('load', function(){
+    
+    //Guarda display em uma variável
+    let display = document.querySelector('#display');
 
-    //Array que receberá os eventos de click
-    let listenerBtn = [];
+    //Guarda botões em variável
+    let reset = document.querySelector('#reset');
+    let limp = document.querySelector('#limpar');
+    let result = document.querySelector('#operadorIgual');
 
-    //Teclas de ferramentas
-    const reset = document.querySelector('#reset');
-    const limpar = document.querySelector('#limpar');
-    const igual = document.querySelector('#operadorIgual');
-    const virgula = document.querySelector('#virgula');
+    //Guarda operadores em um Array
+    let operBtn = [];
+    operBtn.push(document.querySelector('#operadorAd'));
+    operBtn.push(document.querySelector('#operadorSub'));
+    operBtn.push(document.querySelector('#operadorMulti'));
+    operBtn.push(document.querySelector('#operadorDiv'));
 
-    //Preencher array com operadores
-    listenerBtn.push(virgula);
-    listenerBtn.push(document.querySelector('#operadorAd'));
-    listenerBtn.push(document.querySelector('#operadorSub'));
-    listenerBtn.push(document.querySelector('#operadorMulti'));
-    listenerBtn.push(document.querySelector('#operadorDiv'));
-
-    //Preencher array com números
-    listenerBtn.push(document.querySelector('#num0'));
-    listenerBtn.push(document.querySelector('#num1'));
-    listenerBtn.push(document.querySelector('#num2'));
-    listenerBtn.push(document.querySelector('#num3'));
-    listenerBtn.push(document.querySelector('#num4'));
-    listenerBtn.push(document.querySelector('#num5'));
-    listenerBtn.push(document.querySelector('#num6'));
-    listenerBtn.push(document.querySelector('#num7'));
-    listenerBtn.push(document.querySelector('#num8'));
-    listenerBtn.push(document.querySelector('#num9'));
-
-    //Adiciona na tela
-    listenerBtn.forEach((item, index) => {
-        item.addEventListener('click', function(){
-            tela.value += `${listenerBtn[index].value}`;
+    //Guardar números em um Array
+    let numBtn = [];
+    numBtn.push(document.querySelector('#virgula'));
+    numBtn.push(document.querySelector('#num0'));
+    numBtn.push(document.querySelector('#num1'));
+    numBtn.push(document.querySelector('#num2'));
+    numBtn.push(document.querySelector('#num3'));
+    numBtn.push(document.querySelector('#num4'));
+    numBtn.push(document.querySelector('#num5'));
+    numBtn.push(document.querySelector('#num6'));
+    numBtn.push(document.querySelector('#num7'));
+    numBtn.push(document.querySelector('#num8'));
+    numBtn.push(document.querySelector('#num9'));
+    
+    //Adiciona click nos operadores
+    for(let i = 0; i < operBtn.length; i++){
+        operBtn[i].addEventListener("click", function(){
+            adicionarTela();
+            
         });
-    });
+    };
+    
+    //Adiciona click nos numeros
+    for(let j = 0; j < numBtn.length; j++){
+        numBtn[j].addEventListener("click", adicionarTela);
+    };
+    
+    //Adiciona no display
+    function adicionarTela(){
+        display.value += event.target.value;
+    }
+    
+    //Apaga o digito anterior
+    function apagarAnterior() {
+        if (display.value.length > 0) {
+            var aux = display.value.substring(0, display.value.length - 1);
+            display.value = aux;
+        }
+    }
+    
+    //Define operador
+    function verificarOperador(valor) {
+        switch (valor) {
+            case "+":
+                return true;
+            case "-":
+                return true;
+            case "*":
+                return true;
+            case "/":
+                return true;
 
-    //Limpa a tela
-    reset.addEventListener('click', function(){
-        tela.value = '';
-    });
+            default:
+                return false;
+        }
+    }
 
-    //Apaga o valor anterior
-    limpar.addEventListener('click', function(){
-            if (tela.value.length > 0) {
-                let aux = tela.value.substring(0, tela.value.length - 1);
-                tela.value = aux;
+    function verificarResultado() {
+        try{
+            var aux = display.value.substring(display.value.length - 1, display.value.length);
+            if(verificarOperador(aux)){
+                apagarAnterior()
             }
+            
+            
+            var valorCalculado = eval(display.value); //calcular o conteúdo da string
+            if (valorCalculado || valorCalculado == "0") {
+                display.value = valorCalculado;
+            } else {
+                throw "erro";
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    //Calcula valor
+    result.onclick = function (){
+        verificarResultado();
+    }
+
+    //Limpa calculadora
+    reset.addEventListener("click", function(){
+        display.value = '';
     });
 
+    //Limpa digito
+    limp.addEventListener("click", function(){
+        apagarAnterior();
+    })
 });
